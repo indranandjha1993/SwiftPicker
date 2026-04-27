8649719708b83f5bde390f0938e3196259a2d3c5
Indra Nand Jha <indranandjha0@gmail.com>
2026-04-28 00:12:15 +0530

    chore: add Package.swift for command line building

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-interface-format-version: 1.0
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "SwiftPicker",
+    platforms: [
+        .macOS(.v13)
+    ],
+    products: [
+        .executable(name: "SwiftPicker", targets: ["SwiftPicker"])
+    ],
+    targets: [
+        .executableTarget(
+            name: "SwiftPicker",
+            dependencies: [],
+            path: "Sources/SwiftPicker",
+            resources: [
+                .process("../../Resources/Info.plist")
+            ]
+        )
+    ]
+)
